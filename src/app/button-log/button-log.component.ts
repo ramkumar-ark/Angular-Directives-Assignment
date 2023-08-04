dfbd6dd7b7d2c2f1308c78b315f8bb0fdc9b8e3c
Ram Kumar <ramgambit@gmail.com>
2023-08-04 18:08:36 +0530

    Assignment Completed

--- src/app/button-log/button-log.component.ts
+++ src/app/button-log/button-log.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-button-log',
   templateUrl: './button-log.component.html',
-  styleUrls: ['./button-log.component.css']
+  styleUrls: ['./button-log.component.css'],
 })
 export class ButtonLogComponent {
+  displayStatus: boolean = false;
+  logArray: string[] = [];
+  onDisplay(): void {
+    this.displayStatus = !this.displayStatus;
+    this.logArray.push(`Button Clicked at ${Date.now()}`);
+  }
+  getBgColor(log: string): string {
+    const elementIndex: number = this.logArray.indexOf(log);
+    return elementIndex >= 4 ? 'blue' : 'transparent';
+  }
 
+  isTextColor(log: string): boolean {
+    return this.logArray.indexOf(log) >= 4;
+  }
 }
